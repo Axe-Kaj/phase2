6b7500975a58dadcbc79d4b5d051067f293ac2ec
Axe <175651147+Axe-Kaj@users.noreply.github.com>
2026-04-06 21:48:12 -0400

    Add files via upload

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+$server = 'localhost';
+$port = '3306';
+$username = 'root';
+$password = '';
+$database = 'rentruck';
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
